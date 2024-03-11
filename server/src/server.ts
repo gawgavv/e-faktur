87772995e572b8cd95e1f864f757ebeb5b74b767
gawgavv <paramartasukma@gmail.com>
2024-03-11 23:28:38 +0700

    chore: configuring server environment variables

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,14 +6,22 @@ class Server {
     private PORT: number | string;
 
     constructor() {
-        this.PORT = process.env.PORT || 3000;
+        // Server configurations
         this.app = express();
+        this.PORT = process.env.PORT || 3000;
+
+        // Server's requests' body parsers
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({ extended: true }));
     }
 
+    // Server bootstrap method
     public start() {
-        this.app.get(`/`, (req, res) => res.send(`Hello World`))
+        this.app.get(`/`, (req, res) => res.send(`Hello World`));
         this.app.listen(this.PORT, () => console.log(`Server is listening on port ${this.PORT}`));
     }
 }
 
+import dotenv from 'dotenv';
+dotenv.config()
 export default new Server();
