72cd33ffa0e00e3b65686d0dc674eef07cec87c1
gawgavv <paramartasukma@gmail.com>
2024-03-14 00:12:59 +0700

    fix: env variables undefined

--- server/src/configs/cache.ts
+++ server/src/configs/cache.ts
@@ -1,12 +1,15 @@
-import { createClient, RedisClientType } from 'redis';
+import { createClient } from 'redis';
+import dotenv from 'dotenv';
 
 export class Cache {
 
-    public client;
+    private client;
 
+    public connection: ReturnType<typeof this.client.connect>;
     public REVOKED_ACC_TOKEN_KEY = `Token/Revoked`;
 
     constructor() {
+        dotenv.config();
         this.client = createClient({
             password: process.env.CACHE_PASS,
             socket: {
@@ -14,6 +17,11 @@ export class Cache {
                 port: Number(process.env.CACHE_PORT)
             }
         });
+    }
+
+    public async getConnection(): Promise<ReturnType<typeof this.client.connect>> {
+        if(this.connection) return this.connection;
+        return await this.client.connect()
     }
 }
 
